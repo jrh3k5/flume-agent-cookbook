bc7f30578b2c4a6c079fd8de9414fb3805b73dff
Joshua Hyde <joshua.hyde@cerner.com>
2015-09-18 16:56:37 -0500

    Update to 1.1.1 for next development iteration

--- cookbooks/flume_agent/metadata.rb
+++ cookbooks/flume_agent/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email   "jrh3k5@gmail.com"
 license            "Apache 2.0"
 description        "A cookbook used to set up a Flume agent."
 long_description   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version            "1.1.0"
+version            "1.1.1"
